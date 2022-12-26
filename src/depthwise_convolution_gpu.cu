d178348253ca5f1b78e12c53d0ced8eb57a1096e
Sanghyun Woo <shwoo93@kaist.ac.kr>
2022-12-26 08:45:16 +0000

    Added Copyright Headers

--- src/depthwise_convolution_gpu.cu
+++ src/depthwise_convolution_gpu.cu
@@ -1,3 +1,10 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 #include <iostream>
 #include "coordinate_map.hpp"
 #include "coordinate_map_key.hpp"
