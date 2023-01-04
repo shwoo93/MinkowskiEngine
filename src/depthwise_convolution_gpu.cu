c2631d6ef872841c9a008594d09c651e15177c14
Sanghyun Woo <shwoo93@kaist.ac.kr>
2023-01-04 05:43:42 +0000

    Fix Comment Error

--- src/depthwise_convolution_gpu.cu
+++ src/depthwise_convolution_gpu.cu
@@ -1,9 +1,11 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
+/* 
+Copyright (c) Meta Platforms, Inc. and affiliates.
 
-# All rights reserved.
+All rights reserved.
 
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+*/
 
 #include <iostream>
 #include "coordinate_map.hpp"
--- src/depthwise_convolution_kernel.cuh
+++ src/depthwise_convolution_kernel.cuh
@@ -1,9 +1,11 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
+/* 
+Copyright (c) Meta Platforms, Inc. and affiliates.
 
-# All rights reserved.
+All rights reserved.
 
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+*/
 
 #ifndef DEPTHWISE_CONVOLUTION_CUH
 #define DEPTHWISE_CONVOLUTION_CUH
